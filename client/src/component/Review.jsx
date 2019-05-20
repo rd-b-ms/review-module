7ae823faaa0301645447f5e9d5c7c928c4e69f3f
Spencer Pierce <spencer82188@gmail.com>
2019-05-19 18:25:13 -0700

    Rounded user profile pictures and line up the username and time made with the profille image. Formatted the message itself with correct margins and fonts. Spaced the reviews out to match the real Airbnb spacing

--- client/src/component/Review.jsx
+++ client/src/component/Review.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const UserProfilePicture = styled.img`
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+`;
+const UsernameStyle = styled.p`
+  font-weight: 600;
+  margin-bottom: 0px;
+  margin-top: 0px;
+  font-size: 16px;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+  line-height: 1.375em;
+  color: #484848;
+`;
+const MessageStyle = styled.div`
+  color: #484848;
+  margin-top: 16px;
+`;
+const TimeAndUsernameContainer = styled.div`
+  margin-left: 16px;
+`;
+const TimeUsernameProfilePicContainer = styled.div`
+display: inline-flex;
+width: 100%;
+border-spacing: 0px;
+`;
+const TimeStyle = styled.div`
+margin: 0px;
+font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+font-size: 14px;
+font-weight: 400;
+line-height: 1.2857142857142858em;
+color: #484848;
+`;
+const ReviewContainer = styled.div`
+  margin-bottom: 35px;
+`;
 class Review extends React.Component {
   constructor(props) {
     super(props);
@@ -12,18 +50,20 @@ class Review extends React.Component {
       profile_pic_url, username, time_made, message 
     } = review;
     return (
-      <div>
+      <ReviewContainer>
         <div>
-          <div>
-            <div><img src={profile_pic_url} alt="" /></div>
-            <div>{username}</div>
-            <div>{time_made}</div>
-          </div>
+          <TimeUsernameProfilePicContainer>
+            <UserProfilePicture src={profile_pic_url} alt="" />
+            <TimeAndUsernameContainer>
+              <div>
+                <UsernameStyle>{username}</UsernameStyle>
+                <TimeStyle>{time_made}</TimeStyle>
+              </div>
+            </TimeAndUsernameContainer>
+          </TimeUsernameProfilePicContainer>
         </div>
-        <h5>
-          {message}
-        </h5>
-      </div>
+        <MessageStyle>{message}</MessageStyle>
+      </ReviewContainer>
     );
   }
 }
