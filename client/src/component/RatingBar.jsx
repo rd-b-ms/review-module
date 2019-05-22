f1ccc50883778b2a63cd73bd06e8ca04045b9b71
Spencer Pierce <spencer82188@gmail.com>
2019-05-21 19:12:41 -0700

    Removed the styled components from RatingBar.jsx and made a componentsStyles folder that holds the styled componentsfrom RatingBar.jsx

--- client/src/component/RatingBar.jsx
+++ client/src/component/RatingBar.jsx
@@ -1,65 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
+import styles from '../componentsStyles/RatingBarStyles.jsx';
 import Search from './Search.jsx';
 import StarRating from './StarRating.jsx';
 
-const StyledTotalReviewsAndSearch = styled.h2`
-  display: inline-flex;
-`;
-const StyledSearchBar = styled.div`
-  margin-left: 10px;
-  padding-left: 8px;
-`;
-const StyledTotalReviews = styled.div`
-font-size: 24px !important;
-font-weight: 800 !important;
-line-height: 1.25em !important;
-color: #484848 !important;
-padding-top: 2px !important;
-padding-bottom: 2px !important;
-font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
-`;
-const StyledRatingsBox1 = styled.div`
-color: #484848;
-float: left;
-font-family: Circular, -apple-system, system-ui, Roboto, "Helvetica Neue", sans-serif;
-font-size: 14px;
-height: 105px;
-line-height: 20.02px;
-min-height: 1px;
-padding-left: 8px;
-padding-right: 8px;
-width: 332px;
-`;
-const StyledRating = styled.div`
-  margin: 0px;
-  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 1.375em;
-  color: #484848;
-  display: -webkit-box;
-`;
-const StyledRatingsBox2 = styled.div`
-  color: #484848;
-  float: left;
-  font-family: Circular,-apple-system,system-ui,Roboto,"Helvetica Neue",sans-serif;
-  font-size: 14px;
-  height: 105px;
-  line-height: 20.02px;
-  min-height: 1px;
-  padding-left: 8px;
-  padding-right: 8px;
-  width: 332px;
-`;
-const StyledRatingNameAndStars = styled.div`
-  padding-left: 8px;
-  padding-right: 8px;
-  min-height: 1px;
-  position: relative;
-  width: 50%;
-  // float: left;
-`;
+
 class ReviewBar extends React.Component {
   constructor(props) {
     super(props);
@@ -67,6 +11,15 @@ class ReviewBar extends React.Component {
   }
 
   render() {
+    const {
+      StyledTotalReviewsAndSearch,
+      StyledSearchBar,
+      StyledTotalReviews,
+      StyledRatingsBox1,
+      StyledRating,
+      StyledRatingsBox2,
+      StyledRatingNameAndStars,
+    } = styles;
     const { ratings } = this.props;
     let total = 0;
     const totalRatings = {
