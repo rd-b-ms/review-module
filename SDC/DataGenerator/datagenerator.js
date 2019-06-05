9f3a27471f3d500fb1f43cda44209228e3e6d411
Roger Dunn <rrdunn@gmail.com>
2019-06-05 11:23:40 -0700

    Complete seeding file

---
+++ SDC/DataGenerator/datagenerator.js
@@ -0,0 +1,172 @@
+const faker = require('faker');
+const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+var writer = csvWriter();
+var writer1 = csvWriter();
+var writer2 = csvWriter();
+
+
+const randomDataGenerator = () => {
+  return {
+    time_made:faker.date.month() + " " + faker.random.number({min: 2016, max: 2018}), //0 time_made
+    accuracy_rating:faker.random.number({min: 1, max: 5}), //1 accuracy_rating
+    communication_rating:faker.random.number({min: 1, max: 5}), //2 communication_rating
+    cleanliness_rating:faker.random.number({min: 1, max: 5}), //3 cleanliness_rating
+    location_rating:faker.random.number({min: 1, max: 5}), //4 location_rating
+    check_in_rating:faker.random.number({min: 1, max: 5}), //5 check_in_rating
+    value_rating:faker.random.number({min: 1, max: 5}), //6 value_rating
+    message:faker.lorem.paragraph(faker.random.number({min: 1, max: 5})), //7 message
+    listing_id:faker.random.number({min: 1, max: 10}), //8 listing_id
+    user_id:faker.random.number({min: 1, max: 9}), //9 user_id
+  }
+};
+
+const randomUsersGenerator = (numbOfUsers) => {
+  let users = [];
+  writer.pipe(fs.createWriteStream('./dataSets/usersTable.csv'));
+  for (let i = 0; i < numbOfUsers; i++) {
+    let user = {
+      username: faker.name.firstName(), //10 username
+      profile_pic_url: faker.image.avatar(), //11 profile_pic_url
+    };
+    users.push(user);
+    writer.write(user);
+  }
+  writer.end();
+  return users;
+}
+
+var users = randomUsersGenerator(10)
+
+
+const saveToFile = (numbOfReviews) => {
+  writer1.pipe(fs.createWriteStream('./dataSets/fakeDataSQL.csv'));
+  writer2.pipe(fs.createWriteStream('./dataSets/fakeDataNoSQL.csv'));
+  for (let i = 0; i < numbOfReviews; i++) {
+    let randomData = randomDataGenerator();
+    console.log(randomData)
+    writer1.write(randomData);
+    var user_id = randomData.user_id;
+    delete randomData.user_id;
+    randomData.username = users[user_id].username;
+    randomData.profile_pic_url = users[user_id].profile_pic_url;
+    console.log(randomData)
+    writer2.write(randomData);
+  }
+  writer1.end();
+  writer2.end();
+}
+
+saveToFile(10)
+
+// const randomDataGenerator = () => {
+//   return [
+//     faker.date.month() + " " + faker.random.number({min: 2016, max: 2018}), //0 time_made
+//     faker.random.number({min: 1, max: 5}), //1 accuracy_rating
+//     faker.random.number({min: 1, max: 5}), //2 communication_rating
+//     faker.random.number({min: 1, max: 5}), //3 cleanliness_rating
+//     faker.random.number({min: 1, max: 5}), //4 location_rating
+//     faker.random.number({min: 1, max: 5}), //5 check_in_rating
+//     faker.random.number({min: 1, max: 5}), //6 value_rating
+//     faker.lorem.paragraph(faker.random.number({min: 1, max: 5})), //7 message
+//     faker.random.number({min: 1, max: 10}), //8 listing_id
+//     faker.random.number({min: 1, max: 9}), //9 user_id
+//   ]
+// };
+
+// const randomUsersGenerator = (numbOfUsers) => {
+//   let users = [];
+//   for (let i = 0; i < numbOfUsers; i++) {
+//     let newUser = [
+//       faker.name.firstName(), //10 username
+//       faker.image.avatar(), //11 profile_pic_url
+//     ];
+//     users.push(newUser);
+//     if (i === 0) {
+//       fs.writeFileSync('usersTable.csv', newUser + "\n", (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//     } else {
+//       fs.appendFileSync('usersTable.csv', newUser + "\n", (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//     }
+//   }
+//   return users;
+// };
+
+// //change number of users
+// const userArr = randomUsersGenerator(10);
+
+// const saveToFile = (numbOfReviews) => {
+//   for (let i = 0; i < numbOfReviews; i++) {
+//     let randomData = randomDataGenerator();
+//     if (i === 0) {
+//       fs.writeFileSync('fakeDataSQL.csv', `${randomData}\n`, (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//       let userID = randomData.pop();
+//       randomData = randomData.concat(userArr[userID].slice(1));
+//       fs.writeFileSync('fakeDataNoSQL.csv', `${randomData}\n`, (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//     } else {
+//       fs.appendFileSync('fakeDataSQL.csv', `${randomData}\n`, (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//       let userID = randomData.pop();
+//       randomData = randomData.concat(userArr[userID].slice(1));
+//       fs.appendFileSync('fakeDataNoSQL.csv', `${randomData}\n`, (err) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//       });
+//     };
+//     }
+// };
+
+// //change number of reviews
+// saveToFile(10);
+
+
+// SQL:
+// id (auto)
+//0 time_made faker.date.past()
+//1 accuracy_rating faker.random.number({min: 1, max: 5})
+//2 communication_rating faker.random.number({min: 1, max: 5})
+//3 cleanliness_rating faker.random.number({min: 1, max: 5})
+//4 location_rating faker.random.number({min: 1, max: 5})
+//5 check_in_rating faker.random.number({min: 1, max: 5})
+//6 value_rating faker.random.number({min: 1, max: 5})
+//7 user_id
+//8 listing_id
+//9 message faker.lorem.paragraph(INT)
+
+// id (auto)
+//10 username faker.name.firstName()
+//11 profile_pic_url faker.image.avatar()
+
+
+// NOSQL:
+// id (auto)
+//0 time_made faker.date.past()
+//1 accuracy_rating faker.random.number({min: 1, max: 5})
+//2 communication_rating faker.random.number({min: 1, max: 5})
+//3 cleanliness_rating faker.random.number({min: 1, max: 5})
+//4 location_rating faker.random.number({min: 1, max: 5})
+//5 check_in_rating faker.random.number({min: 1, max: 5})
+//6 value_rating faker.random.number({min: 1, max: 5})
+//7 listing_id faker.random.number({min: 1, max: 10000})
+//8 message faker.lorem.paragraph(INT)
+//9 username faker.name.firstName()
+//10 profile_pic_url faker.image.avatar()
---
+++ SDC/Postgres/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS reviews;
+
+CREATE DATABASE reviews;
+
+\c reviews;
+
+CREATE TABLE bnbUsers (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  profile_pic_url TEXT
+);
+
+CREATE TABLE bnbReview (
+  id SERIAL PRIMARY KEY,
+  message TEXT NOT NULL,
+  time_made TEXT NOT NULL,
+  accuracy_rating INT NOT NULL,
+  communication_rating INT NOT NULL,
+  cleanliness_rating INT NOT NULL,
+  location_rating INT NOT NULL,
+  check_in_rating INT NOT NULL,
+  value_rating INT NOT NULL,
+  user_id INT NOT NULL REFERENCES bnbUsers(id),
+  listing_id INT NOT NULL 
+);
+
+\COPY bnbUsers (username, profile_pic_url) FROM '/Users/rogerdunn/Desktop/Programming/HRSF117/SDC/Review-module/SDC/DataGenerator/usersTable.CSV' DELIMITERS ',' CSV HEADER;
+\COPY bnbReview (time_made, accuracy_rating, communication_rating, cleanliness_rating, location_rating, check_in_rating, value_rating, message, listing_id, user_id) FROM '/Users/rogerdunn/Desktop/Programming/HRSF117/SDC/Review-module/SDC/DataGenerator/fakeDataSQL.CSV' DELIMITERS ',' CSV HEADER;
